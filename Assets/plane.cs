14e0b7a9a28283b28b3362c0be2c1da3578b7b40
mcshulik <mc-shulik@mail.ru>
2019-07-07 19:42:30 +0300

    Init

---
+++ Assets/plane.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{private void OnTriggerEnter(Collider other)
+    {
+      
+    }
+
+    // stayCount allows the OnTriggerStay to be displayed less often
+    // than it actually occurs.
+    private float stayCount = 0.0f;
+}
